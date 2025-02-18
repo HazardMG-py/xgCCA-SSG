0b2591d7233bedf9ad3357bc65dbc42ff6da94a5
Godwin Okwara <okwaragodwin330@gmail.com>
2025-02-18 14:22:13 -0500

    augmentation

--- xgcca_model.py
+++ xgcca_model.py
@@ -3,6 +3,47 @@ import torch.nn as nn
 import torch.nn.functional as F
 from dgl.nn import GraphConv
 
+class LogReg(nn.Module):
+    def __init__(self, hid_dim, out_dim):
+        super(LogReg, self).__init__()
+        self.fc = nn.Linear(hid_dim, out_dim)
+
+    def forward(self, x):
+        return self.fc(x)
+
+class MLP(nn.Module):
+    def __init__(self, nfeat, nhid, nclass, use_bn=True):
+        super(MLP, self).__init__()
+        self.layer1 = nn.Linear(nfeat, nhid, bias=True)
+        self.layer2 = nn.Linear(nhid, nclass, bias=True)
+        self.bn = nn.BatchNorm1d(nhid)
+        self.use_bn = use_bn
+        self.act_fn = nn.ReLU()
+
+    def forward(self, _, x):
+        x = self.layer1(x)
+        if self.use_bn:
+            x = self.bn(x)
+        x = self.act_fn(x)
+        x = self.layer2(x)
+        return x
+
+class GCN(nn.Module):
+    def __init__(self, in_dim, hid_dim, out_dim, n_layers):
+        super(GCN, self).__init__()
+        self.n_layers = n_layers
+        self.convs = nn.ModuleList()
+        self.convs.append(GraphConv(in_dim, hid_dim, norm='both'))
+        if n_layers > 1:
+            for i in range(n_layers - 2):
+                self.convs.append(GraphConv(hid_dim, hid_dim, norm='both'))
+            self.convs.append(GraphConv(hid_dim, out_dim, norm='both'))
+
+    def forward(self, graph, x):
+        for i in range(self.n_layers - 1):
+            x = F.relu(self.convs[i](graph, x))
+        x = self.convs[-1](graph, x)
+        return x
 
 class BicliqueAttentionLayer(nn.Module):
     def __init__(self, in_dim, out_dim):
